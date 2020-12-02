b1b2feb2f9a77607cdd491b7460161d15f207b4d
Sviridov_Aleksey <agrinipis1@gmail.com>
2020-12-02 21:16:51 +0300

    Java2Practice8Finall

---
+++ Java2Practice8/NetworkClient Server/src/clientserver/commands/EndConnectionMessage.java
@@ -0,0 +1,16 @@
+package clientserver.commands;
+
+import java.io.Serializable;
+
+public class EndConnectionMessage implements Serializable {
+
+    private final String sender;
+
+    public EndConnectionMessage(String sender) {
+        this.sender = sender;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+}
