bd2db54222249145354a7fce3649d461e219fbf8
Sviridov_Aleksey <agrinipis1@gmail.com>
2020-12-06 21:55:28 +0300

    Java3Practice1Finall

---
+++ Java3Practice1/src/Fruit.java
@@ -0,0 +1,14 @@
+public class Fruit {
+
+    private float weight;
+
+    public Fruit(Float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight(){
+        return weight;
+    }
+
+
+}
